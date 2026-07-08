2b5c74b3e9845d6cbfab1f6ac6cb066c7808b525
Manimonisha.Parthsarathy <Manimonisha.Parthsarathy@ducenit.com>
2026-07-08 19:19:16 +0530

    Resume downloads

--- portfolio/src/app/portfolio-contact/portfolio-contact.component.ts
+++ portfolio/src/app/portfolio-contact/portfolio-contact.component.ts
@@ -12,4 +12,11 @@ export class PortfolioContactComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  downloadResume(): void {
+  const link = document.createElement('a');
+  link.href = 'assets/resume/Monisha_Resume.pdf';
+  link.download = 'Monisha_Resume.pdf';
+  link.click();
+}
+
 }
